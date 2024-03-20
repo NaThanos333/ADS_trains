eced14e5ec64bb8395c84c455d8573ea12975b60
NaThanos333 <148961975+NaThanos333@users.noreply.github.com>
2024-03-20 12:23:22 +0100

    Update lab5trains.py

--- lab5trains.py
+++ lab5trains.py
@@ -10,6 +10,8 @@ If it is smaller than the current distance of Node, set it as the new current di
 
 """
 
+# testtesttest
+
 def dijkstra_algorithm_implementation():
     # Step 1:
     non_visited_nodes = [0, 1, 2, 3]

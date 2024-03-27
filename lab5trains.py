a0f01e54d9cd0e3d43dd0200d4bbced63bc9d67b
NaThanos333 <148961975+NaThanos333@users.noreply.github.com>
2024-03-27 10:57:10 +0100

    update

--- lab5trains.py
+++ lab5trains.py
@@ -20,7 +20,7 @@ def dijkstra_algorithm_implementation():
         current_node = node_distances.index(min(node_distances))
 
         # Step 3:
-
+        
 
 
         if len(non_visited_nodes) == 0:
@@ -31,25 +31,3 @@ def dijkstra_algorithm_implementation():
 
 
 dijkstra_algorithm_implementation()
-"""
-class Graph:
-    def __init__(self):
-        self.graph = {}
-
-    def add_edge(self, node1, node2, weight):
-        if node1 not in self.graph:
-            self.graph[node1] = [(node2, weight)]
-        else:
-            self.graph[node1].append((node2, weight))
-
-        if node2 not in self.graph:
-            self.graph[node2] = [(node1, weight)]
-        else:
-            self.graph[node2].append((node1, weight))
-
-    def show_edges(self):
-        for node in self.graph:
-            for neighbour, weight in self.graph[node]:
-                print("(", node, ", ", neighbour, ", weight: ", weight, ")")
-"""
-    

6be076974a182146d36230a62ec7105a7e2470c6
MrTunaPizza <66560754+MrTunaPizza@users.noreply.github.com>
2024-03-27 18:06:55 +0100

    Update CGPTTrains.py

--- CGPTTrains.py
+++ CGPTTrains.py
@@ -1,5 +1,6 @@
 import heapq
 
+
 class Graph:
     def __init__(self):
         self.edges = {}
@@ -8,26 +9,33 @@ class Graph:
         if origin not in self.edges:
             self.edges[origin] = {}
         self.edges[origin][destination] = weight
-        
+
         # Since it's an undirected graph, add the reverse edge as well
         if destination not in self.edges:
             self.edges[destination] = {}
         self.edges[destination][origin] = weight
 
-    def dijkstra(self, start):
+    def dijkstra(self, start, end):
         distances = {node: float('inf') for node in self.edges}
+        predecessors = {node: None for node in self.edges}
         distances[start] = 0
         heap = [(0, start)]
-        while heap:
+        current_node = start
+        while end != current_node:
+            if not len(heap):
+                return None, None
             current_distance, current_node = heapq.heappop(heap)
             if current_distance > distances[current_node]:
                 continue
-            for neighbor, weight in self.edges[current_node].items():
-                distance = current_distance + weight
-                if distance < distances[neighbor]:
-                    distances[neighbor] = distance
-                    heapq.heappush(heap, (distance, neighbor))
-        return distances
+            if current_node in self.edges.keys():
+                for neighbor, weight in self.edges[current_node].items():
+                    distance = current_distance + weight
+                    if distance < distances[neighbor]:
+                        distances[neighbor] = distance
+                        predecessors[neighbor] = current_node
+                        heapq.heappush(heap, (distance, neighbor))
+        return distances, predecessors
+
 
 # Create a graph
 graph = Graph()
@@ -47,14 +55,45 @@ edges = {
     "Leeuwarden": {"Groningen": 34, "Meppel": 40},
     "Meppel": {"Groningen": 49, "Leeuwarden": 40, "Zwolle": 15}
 }
+input_lst = []
+lineinput = input()
+while lineinput != "!":
+    input_lst.append(lineinput)
+    lineinput = input()
+
+# Test Dijkstra's algorithm
+line = 0
+number = int(input_lst[0])
+for n in range(number):
+    line += 1
+    city1 = input_lst[line]
+    line += 1
+    city2 = input_lst[line]
+    if city2 in edges[city1]:
+        del edges[city1][city2]
+    if city1 in edges[city2]:
+        del edges[city2][city1]
 
 for origin, destinations in edges.items():
     for destination, weight in destinations.items():
         graph.add_edge(origin, destination, weight)
 
-# Test Dijkstra's algorithm
-start_node = "Amsterdam"
-distances = graph.dijkstra(start_node)
-print("Shortest distances from", start_node + ":")
-for node, distance in distances.items():
-    print(node + ":", distance)
+while line < len(input_lst):
+    line += 1
+    start_node = input_lst[line]
+    line += 1
+    end_node = input_lst[line]
+    path = []
+    distances, predecessors = graph.dijkstra(start_node, end_node)
+    if distances is None:
+        print("UNREACHABLE")
+    else:
+        final_distance = distances[end_node]
+        while end_node != start_node:
+            path.append(end_node)
+            end_node = predecessors[end_node]
+        path.append(start_node)
+        path.reverse()
+        for i in path:
+            print(i)
+        print(final_distance)
